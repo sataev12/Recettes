c117b5eb5d23e28aa3c6642bc210b32187f688ba
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-26 02:11:40 +0100

    Requêt des nombre ingredients necessaire pour chaque recette

--- requêtes/requette.sql
+++ requêtes/requette.sql
@@ -7,3 +7,11 @@ INNER JOIN Categorie ON Recette.Id_Categorie = Categorie.Id
 ORDER BY TempsPrepa DESC;
 
 -- 2) En modifiant la requête précédente, faites apparaître le nombre d'ingrédients nécessaire par recette.
+
+SELECT Recette.Nom, Recette.TempsPrepa, Categorie.TypePlat, COUNT(relation0.Id_Recette) AS NombreIngredients 
+FROM Recette
+INNER JOIN Categorie ON Recette.Id_Categorie = Categorie.Id
+INNER JOIN relation0 ON Recette.Id = relation0.Id_Recette
+GROUP BY Recette.Id, Recette.Nom  
+ORDER BY TempsPrepa DESC;
+
